909d58e062e592068a57e609c50b7aa4d9758bfc
SaulCandan <69408286+SaulCandan@users.noreply.github.com>
2024-07-01 19:13:42 +0200

    Update actualizarIp.sh

--- actualizarIp.sh
+++ actualizarIp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://freemyip.com/update?token=08804b6d8ec9c53fb0f3f0b3&domain=duplexou.freemyip.com>/dev/null
+curl https://freemyip.com/update?token=<_token_>&domain=duplexou.freemyip.com>/dev/null
 
 toret="Ip Actualizada: ";
 toret="$toret$(nslookup duplexou.freemyip.com | tail -2 | awk '{print $2;}')";
